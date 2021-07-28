6b82403a4206cd3c6a77e5850206bb61a26632d1
ChumSrun <chumsrun@gmail.com>
2021-07-28 09:07:05 +0700

    add google storage ducket

--- pages/api/users/profile-picture.js
+++ pages/api/users/profile-picture.js
@@ -3,7 +3,15 @@ import User from "@/models/User";
 import ErrorResponse from "@/utils/errorResponse";
 import upload from "@/middlewares/uploadFile";
 import nc from "next-connect";
+import { Storage } from "@google-cloud/storage";
+import path from "path";
 const handler = nc();
+
+const storage = new Storage({
+  keyFilename: "moj-hr-39b0f2e19dd4.json.example",
+  // keyFilename: "moj-hr-39b0f2e19dd4.json",
+  projectId: "moj-hr",
+});
 
 handler.use(all);
 
@@ -21,8 +29,14 @@ handler.post(
     if (!req.file) {
       throw new ErrorResponse("Image not found", 400);
     }
+    const userUploadBucket = storage.bucket("user_file_upload");
+    const resUpload = await userUploadBucket.upload(
+      path.join('public', "uploads", "img-profile", req.file.filename),
+      { destination: "img-profile/" + req.file.filename }
+    );
+    console.log(resUpload);
     const user = await User.findById(req.user.id);
-    user.photo = ("/uploads/img-profile/" + req.file.filename).toString();
+    user.photo = ('https://storage.googleapis.com/user_file_upload/img-profile/' + req.file.filename).toString();
     await user.save();
     res.status(200).json({ success: true, data: { user } });
   }
