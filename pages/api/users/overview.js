f9aa6b773c4e7df6fd3b357c9992ca50d0e92005
ChumSrun <chumsrun@gmail.com>
2021-11-23 10:04:18 +0700

    Fix overview api

--- pages/api/users/overview.js
+++ pages/api/users/overview.js
@@ -1,12 +1,13 @@
 import nc from "next-connect";
 import { protect, role } from "@/middlewares/auth";
-const { default: all, errorHandle } = require("@/middlewares/all");
+import { ncOpts } from "api-lib/nc";
+import database from "@/middlewares/database";
+// const { default: all, errorHandle } = require("@/middlewares/all");
 
 const { getOverviewEmployees } = require("controllers/employee");
 
-const handler = nc(errorHandle);
-
-handler.use(all);
+const handler = nc(ncOpts);
+handler.use(database);
 
 handler.use(protect, role("admin", "editor"));
 handler.get(getOverviewEmployees);
